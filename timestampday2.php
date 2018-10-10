73c65cb4f4f4bcae9bd084652744802380b8a5b8
f r e s h <fresh.thit@me.com>
2018-10-11 01:56:12 +0700

    Update timestampday2.php

--- timestampday2.php
+++ timestampday2.php
@@ -33,20 +33,46 @@ mysqli_close($link);
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
+    <title>Register</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
     <script src="main.js"></script>
+
+    <!-- BOOTSTRAP -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
 </head>
-<body>
+    <body>
+        <img class="background non-selectable" src="img/bg.svg" />
 
-<form action="checkqrcode.php" method="post">
-  QR-COCE: <input type="text" name="qrcode" autofocus><br>
-  <input type="submit" value="Submit">
-</form>
+        <div class="main-container">
+            <div class="sponsor-bar non-selectable">
+                <img src="img/kmutt.svg" style="height: 59.2px"/>
+                <img src="img/saturday-school.svg" style="height: 31.5px"/>
+                <img src="img/unicef.svg" style="height: 50px"/>
+            </div>
+            <div class="body">
+                <div class="flex-horizontal">
+                    <div class="flex-vertical" style="padding-top: 10%">
+                        <img src="img/title.svg" class="non-selectable" style="height: 133.9px;"/>
 
+                        <div class="form1">
 
-
-
-</body>
+                            <form action="checkqrcode.php" method="post">
+                                QR-COCE: <input type="text" name="qrcode" autofocus><br>
+                                <input type="submit" value="Submit">
+                            </form>
+                        </div>
+                    </div>
+                    <img src="img/gen-unlimited.svg" class="non-selectable" style="height: 382.5px"/>
+                </div>
+                
+            </div>
+        </div>
+        
+        <!-- REQUIRED SCRIPTS FOR BOOTSTAP -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    </body>
 </html>
